36d3ee3e95e2051043c6101a8c0c31498f5acae0
Charumeran <s.ryosei06@gmail.com>
2024-11-06 15:26:17 +0900

    BaseChange With Router Assignment

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import {BrowserRouter as Router, Route, Routes, Link, BrowserRouter } from "react-router-dom";
+import { Route, Routes, Link, BrowserRouter } from "react-router-dom";
 import HomePage from "./HomePage";
 import { Post } from "./types";
 import PostPage from "./PostPage";
@@ -14,7 +14,7 @@ const App = () => {
     };
 
     return(
-        <BrowserRouter basename={ process.env.NODE_ENV === "production" ? "/Assignment" : "/"}>
+        <BrowserRouter basename={ process.env.NODE_ENV === "production" ? "/Assignment/" : "/"}>
             <div>
                 <nav>
                     <Link to = "/search"></Link>
